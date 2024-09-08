dc6a5f68d68943bce8f5b20d7cebc6e970d0661b
Shaikh Arsalan <shaikharsalan123@gmail.com>
2024-09-08 10:30:38 +0530

    push test2 file

---
+++ test2.js
@@ -0,0 +1,13 @@
+const hero = ["Ironman","superman","Batman"]
+
+let myObj=
+{
+    name : "Arsalan",
+    age : 33,
+}
+
+const myFunction = function()
+{
+console.log("testing my new program")    
+}
+myFunction;
